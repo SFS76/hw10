38ad490a72842b954c5c0b2c95089938e5446f61
SFS76 <sfs76@yandex.ru>
2024-08-12 19:21:03 +0400

    Start

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Task 1
         String firstName= "Ivanov";
         String middleName = "Ivan";
         String lastName = "Ivanovich";
         String fullName = String.join(" ", firstName, middleName, lastName);
-        System.out.printf("Ф. И. О. сотрудника —" + fullName);
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        //Task 2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //Task 3
+        String fullNameRus = "Иванов Семён Семёнович";
+        fullNameRus = fullNameRus.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameRus);
     }
 }
